185acbdcffc0de1952e922882710d42767eec69f
Osmar Valero <osmarvalero100@gmail.com>
2020-05-01 19:14:15 -0500

    Crea modelo User

---
+++ express/models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    isCustomer: Boolean,
+    email: String,
+    date: {type: Date, default: Date.now}
+});
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
